c3ad190b5a9d215721634cf888594f4a3cc2bbaa
emitolaba95 <emi.tolaba95@gmail.com>
2016-09-08 02:51:42 -0300

    Divido las responsabilidades

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -3,37 +3,62 @@ require_relative '../src/Testing'
 
 describe 'TADsPec tests' do
 
-  tester = TADsPec.new
+  verificador = Verificador.new
+  tester = Tester.new
   it 'cualquier objeto entiende :deberia' do
-
     expect(Object.respond_to? (:deberia)).to be(true)
+  end
+
+  it 'cualquier objeto entiende :ser' do
+    expect(Object.respond_to? (:ser)).to be(true)
+  end
+
+  it 'se pasa el test deberia con ser' do
+    leandro = Persona.new
+    leandro.edad = 20
+
+    expect(leandro.edad.deberia ser 15).to be(false)
+  end
+
+  it 'falla el test deberia con ser' do
+    expect(1.deberia ser 1).to be(true)
   end
 
   #TODO: cambiar las aserciones de los tests!
   it 'se testea el contexto' do
-      expect(tester.testear).to be(0)
+      expect(verificador.testear).to be(0)
   end
 
   it 'se testea la suite entera' do
-    expect(tester.testear 1).to be(1)
+    expect(verificador.testear MiSuiteDeTests).to be(1)
   end
 
   it 'se testean solo algunos metodos de la suite' do
-    expect(tester.testear 1,2).to be(2)
+    expect(verificador.testear MiSuiteDeTests, (:testear_que_pasa_algo)).to be(2)
   end
 
   it 'es un test' do
-    expect(TADsPec.es_test? (:testear_que_algo)).to be(true)
+    expect(verificador.es_test? (:testear_que_algo)).to be(true)
   end
 
   it 'se elimina el mensaje deberia' do
-    tester.sacar_deberia
+    verificador.sacar_deberia
     expect(Object.respond_to? (:deberia)).to be(false)
   end
 
   it 'se testea una clase con tests' do
-    expect(TADsPec.contiene_tests MiSuiteDeTests).to be(true)
+    expect(verificador.contiene_tests MiSuiteDeTests).to be(true)
+  end
 
+  it 'se testea un test de la clase con tests' do
+    verificador.testear MiSuiteDeTests, :testear_que_pasa_algo
+    expect(verificador.resultado.tests_corridos).to be(1)
+  end
+
+  it 'se define el test tener_' do
+    leandro = Persona.new
+    leandro.deberia tener_edad 22
+    expect(leandro.respond_to? (:tener_edad)).to be(true)
   end
 
 end
--- src/Testing.rb
+++ src/Testing.rb
@@ -1,52 +1,27 @@
-class TADsPec
-
-  #include Ser
-
-  def definir_deberia
-    #defino el metodo deberia a todos los objetos
-    Object.send(:define_method, (:deberia), proc do |procedimiento| self.instance_eval &procedimiento end)
-  end
-
-  def definir_ser
-   Object.send(:define_method, (:ser), proc do |resultado| self.eql? resultado end)
-  end
+class Tester
 
   def initialize
-    definir_deberia
-    definir_ser
+    resultado= Resultado.new
   end
 
-  def testear(*args)
-    tamanio = args.size
-    if(tamanio == 0)
-      'Testeando el contexto'
-      0
-    else
-      if (tamanio == 1)
-        1
-      else
-        2
-      end
+  def corre_test(test)
+    "blabla"
+  end
+
+  def corre_conjunto_de_tests(*args)
+    args.each do |test|  corre_test test
     end
   end
 
-  def self.es_test?(sym)
-    sym.to_s[0..11] == 'testear_que_'
-  end
-
-  def validar(sym)
-    if es_test? sym
-      #testear_metodo
+  def correr_suite(clase)
+    clase.methods(regular=true).each do |metodo|
+      self.corre_test metodo
     end
   end
 
-  def sacar_deberia
-    Object.send(:undef_method, :deberia)
-  end
-
-
-  def method_missing(sym, *args)
-    if(sym.to_s.size > 3)
+  def correr_metodos_de(clase, *args)
+    if(args.all? {|metodo| clase.respond_to? metodo})
+       self.corre_conjunto_de_tests args
     end
   end
 
@@ -68,17 +43,78 @@ class TADsPec
 
     end
   end
+end
 
-  def self.contiene_tests(una_clase)
+class Verificador
+
+  attr_accessor :tester, :resultado
+
+  def definir_deberia
+    #defino el metodo deberia a todos los objetos
+    Object.send(:define_method, (:deberia), proc { |proc| instance_eval &proc})
+  end
+
+  def definir_ser
+    # Object.send(:define_method, (:ser), Proc.new do |resultado| self.eql? resultado end)
+    Object.send(:define_method, (:ser), proc { |resultado|
+      Proc.new do
+        self.equal? resultado
+      end })
+
+  end
+
+  def es_test?(sym)
+    sym.to_s[0..11] == 'testear_que_'
+  end
+
+
+  def contiene_tests(una_clase)
     var = una_clase.new
-    metodos = var.methods(regura=true).select do |elem|
-      TADsPec.es_test? elem
+    metodos = var.methods(regular=true).select do |elem|
+      self.es_test? elem
     end
     not metodos.empty?
   end
 
 
+  def validar(sym)
+    if es_test? sym
+      #testear_metodo
+    end
+  end
+
+  def testear(*args)
+    tamanio = args.size
+    tester= Tester.new
+    resultado= Resultado.new
+    if(tamanio == 0)
+      #corre_test(args.first)
+      0
+    else if (tamanio == 1)
+           tester.correr_suite(args.first)
+           resultado.tests_corridos = 1
+           1
+         else
+           tester.corre_conjunto_de_tests(args[1..-1])
+           2
+         end
+    end
+  end
+
+
+  def sacar_deberia
+    Object.send(:undef_method, :deberia)
+  end
+
+  def initialize
+    tester= Tester.new
+    definir_deberia
+    definir_ser
+  end
+
 end
+
+
 
 class MiSuiteDeTests
   def testear_que_pasa_algo
@@ -87,6 +123,28 @@ class MiSuiteDeTests
 
   def otro_metodo_que_no_es_un_test
   end
+end
 
+class Resultado
+  attr_accessor :tests_corridos, :tests_pasados, :tests_explotados
+
+  def initialize
+    tests_corridos = 0
+    tests_pasados =0
+    tests_explotados =0
+  end
+
+end
+
+class Persona
+  attr_accessor :edad
+end
+
+class PersonaTest
+  def testear_que_se_use_la_edad
+    lean = Persona . new ( 22)
+    pato = Persona . new ( 23)
+
+  end
 
 end
